d1d6c484125f5c9c538eda53c58167b67be23b72
Pyunghyuk Yoo <pyunghyuk@likelion.net>
2023-01-09 17:15:59 +0900

    ADD: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const packageJson = JSON.parse(
+    fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(packageJson.name)
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
